84d3b84e4498bd430382dcc0b141365c1b732a00
Muhammad Taufiqulsadi <muhammad.taufiqulsadi@shipper.id>
2023-11-23 08:32:06 +0700

    feat(init): connect env

--- app.go
+++ app.go
@@ -6,13 +6,16 @@ import (
 	"github/metaufiq/mobile-legend-hero-service/src/hero"
 	"net/http"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	godotenv.Load()
 	http.HandleFunc("/", hero.GetRoot)
 	http.HandleFunc("/hero", hero.GetHero)
 	fmt.Printf("server running...\n")
-	err := http.ListenAndServe(":3333", nil)
+	err := http.ListenAndServe(":8080", nil)
 	if errors.Is(err, http.ErrServerClosed) {
 		fmt.Printf("server closed\n")
 	} else if err != nil {
