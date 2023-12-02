3711e2216a11329f599a8bdd4638cdee97875e69
Muhammad Taufiqulsadi <muhammad.taufiqulsadi@shipper.id>
2023-12-02 10:32:19 +0700

    feat(init): implement query database

--- src/common/database.go
+++ src/common/database.go
@@ -9,14 +9,25 @@ import (
 	_ "github.com/lib/pq"
 )
 
-func InnitDB() {
+var db *sql.DB
+
+func InnitDB() *sql.DB {
+	if db != nil {
+		fmt.Printf("\nuse existing connection..\n")
+		return db
+	}
 	fmt.Printf("\nrun database from ENV...\n")
+
+	dbLocation := os.Getenv("DB_LOCATION")
 
 	userName := os.Getenv("DB_USERNAME")
 	password := os.Getenv("DB_PASSWORD")
-	url := os.Getenv("DB_URL")
-	connStr := fmt.Sprintf("postgresql://%s:%s@%s", userName, password, url)
-	_, err := sql.Open("postgres", connStr)
+	dbName := os.Getenv("DB_NAME")
+	dbPort := os.Getenv("DB_PORT")
+	connStr := fmt.Sprintf("host=%s port=%s user=%s "+
+		"password=%s dbname=%s sslmode=disable", dbLocation, dbPort,
+		userName, password, dbName)
+	conn, err := sql.Open("postgres", connStr)
 
 	if err != nil {
 		fmt.Printf("\nfound an error when try to connect to database:\n")
@@ -24,5 +35,7 @@ func InnitDB() {
 		fmt.Print("\n")
 	}
 
-	fmt.Printf("\nSuccess run database from %s\n", url)
+	db = conn
+
+	return db
 }
--- src/presentation/routes.go
+++ src/presentation/routes.go
@@ -1,20 +1,47 @@
 package presentation
 
 import (
+	"fmt"
 	"github/metaufiq/mobile-legend-hero-service/src/common"
 	"net/http"
 )
 
 func getRoot(w http.ResponseWriter, r *http.Request) {
 	resp := make(map[string]string)
-	resp["data"] = "this is root page, to get heroes data you can access /hero"
+	resp["data"] = "cannot access your data here"
 
 	common.CreateJSONResponse(w, resp)
 }
 
 func getHero(w http.ResponseWriter, r *http.Request) {
-	resp := make(map[string]string)
-	resp["data"] = "not available"
+	resp := make(map[string]any)
+	db := common.InnitDB()
+	rows, err := db.Query("SELECT * FROM hero_counter")
+
+	if err != nil {
+
+		fmt.Println(err.Error())
+		return
+	}
+
+	var data []map[string]int
+	for rows.Next() {
+		var id int
+		var heroId int
+		var counterHeroId int
+		err = rows.Scan(&id, &heroId, &counterHeroId)
+		if err != nil {
+			fmt.Println(err.Error())
+			break
+		}
+		hero := map[string]int{
+			"id":            id,
+			"heroId":        heroId,
+			"counterHeroId": counterHeroId,
+		}
+		data = append(data, hero)
+	}
+	resp["data"] = data
 
 	common.CreateJSONResponse(w, resp)
 }
